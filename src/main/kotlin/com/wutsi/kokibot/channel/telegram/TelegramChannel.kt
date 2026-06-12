f8eec4c590326b0739c2be4faaf94423cbf51440
wutsi <htchepannou@expediagroup.com>
2026-06-12 08:44:45 -0400

    make telegram response concise

--- src/main/kotlin/com/wutsi/kokibot/channel/telegram/TelegramChannel.kt
+++ src/main/kotlin/com/wutsi/kokibot/channel/telegram/TelegramChannel.kt
@@ -239,10 +239,7 @@ class TelegramChannel(
         try {
             return context.assistant.process(
                 Message(
-                    text = update.message.text +
-                        "\n\nTelegram does not support tables." +
-                        "Use bullet points instead, " +
-                        "but if you really have to render information using table, wrap it within a code block (e.g., ``` ... ```) and make sure that cells a properly spaced.",
+                    text = decorateQuery(update.message.text),
                     role = Role.USER,
                     userId = userId,
                     channelId = id(),
@@ -286,6 +283,21 @@ class TelegramChannel(
             }
         }
     }
+
+    private fun decorateQuery(query: String): String {
+        return if (query.startsWith("/")) {
+            query
+        } else {
+            query + "\n\n" +
+                """
+                    Since this communication channel is a chat, the answer should be concise and to the point. Avoid unnecessary explanations or verbose language.
+                    - If the query is a command or requires a concise answer, keep the response under 50 words.
+                    - If the query is a question or a request for explanation, provide a clear and informative response within 150 to 200 words.
+                    - Never us tables to format information, use bullet points instead; but if you really have to render information using tables, ALWAYS wrap it within a code block (``` ... ```) and make sure that cells a properly spaced.
+                """.trimIndent()
+        }
+    }
+
 
     private fun toUserId(update: Update): String {
         val chat = update.message.chat
